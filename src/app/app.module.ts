3d48abf52f94f21bc17ce3f91348e57004342034
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-27 00:33:13 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ViewComponent } from './view/view.component';
+
+const myRoute:Routes=[
+
+  {
+    path:"",
+    component:AddComponent
+  },
+  {
+    path:"view",
+    component:ViewComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
